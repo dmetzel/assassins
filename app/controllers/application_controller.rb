b21f87a3a7e863d557fab5812b034c63471788af
dmetzel <dmetzel2016@kellogg.northwestern.edu>
2015-06-09 20:04:15 -0700

    Before removing kill confirm

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   before_filter :authenticate_user!
 
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :confirm_kill
+  #before_action :confirm_kill
 
   protected
 
